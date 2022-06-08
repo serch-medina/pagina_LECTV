2837093760488e9871f19d08746483c5b86824f0
Sergio Javier <320334908@alumno.enp.unam.mx>
2022-06-08 14:08:12 -0500

    Todos los arhivos commiteados

---
+++ js/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'NBo1Pxgi2Okoxlc9p1kv8RPGE');
+    define('CONSUMER_SECRET', 'Hn7tfnXdAOvY1VpWEZmWmdH9OHO1HyD6dEvOECdUX7FOrhoUss');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '3171664794-BL9mO3Ru4ulbXqHZ20PTZFC3tksoy40XaNXWmom');
+    define('ACCESS_SECRET', 'ncfrd6seRQVXUrbEAjm8wFj0W4vxhGxn3wnaQfh6ceTFO');
+    
+    // Cache Settings
+    define('CACHE_ENABLED', false);
+    define('CACHE_LIFETIME', 3600); // in seconds
+    define('HASH_SALT', md5(dirname(__FILE__)));
